ed130f0e349f4dc676c3697026fa752a6217690a
vcabieles <victorcabieles@gmail.com>
2017-04-27 23:03:05 -0400

    remove file from git ignore

---
+++ config.js
@@ -0,0 +1,6 @@
+ var config = {
+     secret: "hello_dsfas_fasdfasd3423REWR$#kjasdfhklaj*(**(*HKFJ4165489",
+     database: "mongodb://67.205.187.131:27017/userbase"
+ };
+
+ module.exports = config;
